366965d6b426b19f067d09a9a67e1499babb1bb4
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-10-22 13:50:49 +0700

    Update main.cpp

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -1,16 +1,16 @@
 #include "main.h"
 
-//TASK опредёлен в pro иначе жалуется "macro name missing"
+//TASK ╨╛╨┐╤А╨╡╨┤╤С╨╗╨╡╨╜ ╨▓ pro ╨╕╨╜╨░╤З╨╡ ╨╢╨░╨╗╤Г╨╡╤В╤Б╤П "macro name missing"
 static int var{12};
 
 int main()
 {
 #if (TASK==1)
-    cout << "Задание 1" <<"\n";
+    cout << "╨Ч╨░╨┤╨░╨╜╨╕╨╡ 1" <<"\n";
     task1();
 #endif
 #if (TASK==2)
-    cout << "Задание 2" <<"\n";
+    cout << "╨Ч╨░╨┤╨░╨╜╨╕╨╡ 2" <<"\n";
     printChar();
     printChar('q');
     printChar('q','w');
@@ -18,31 +18,31 @@ int main()
     printChar('q','w','e','r');
 #endif
 #if (TASK==3)
-    cout << "Задание 3"<<"\n";
+    cout << "╨Ч╨░╨┤╨░╨╜╨╕╨╡ 3"<<"\n";
     cout<<Initialization()<<'\n';
     int var=33;
-    cout<<"Глобальная - "<<::var<<"; Локальная - "<<var;
+    cout<<"╨У╨╗╨╛╨▒╨░╨╗╤М╨╜╨░╤П - "<<::var<<"; ╨Ы╨╛╨║╨░╨╗╤М╨╜╨░╤П - "<<var;
 #endif
 #if (TASK==4)
-    std::cout << "Задание 4" <<"\n";
+    std::cout << "╨Ч╨░╨┤╨░╨╜╨╕╨╡ 4" <<"\n";
     Animal zoo=goat;
     getAnimalName(zoo);
     printNumberOfLegs(zoo);
 #endif
 #if (TASK==5)
-    std::cout << "Задание 5" <<"\n";
-    std::cout <<"Введите 3 числа:\n";
+    std::cout << "╨Ч╨░╨┤╨░╨╜╨╕╨╡ 5" <<"\n";
+    std::cout <<"╨Т╨▓╨╡╨┤╨╕╤В╨╡ 3 ╤З╨╕╤Б╨╗╨░:\n";
     int v[3];
     ChekValue(v,3);
-    std::cout << "Максимум : "<<SeekMax(v[0],v[1],v[2])<<'\n';
-    std::cout << "Минимум : "<<SeekMin(v[0],v[1],v[2])<<'\n';
+    std::cout << "╨Ь╨░╨║╤Б╨╕╨╝╤Г╨╝ : "<<SeekMax(v[0],v[1],v[2])<<'\n';
+    std::cout << "╨Ь╨╕╨╜╨╕╨╝╤Г╨╝ : "<<SeekMin(v[0],v[1],v[2])<<'\n';
 #endif
 #if (TASK==6)
-    std::cout << "Задание 6" <<"\n";
+    std::cout << "╨Ч╨░╨┤╨░╨╜╨╕╨╡ 6" <<"\n";
     task6();
 #endif
 #if (TASK==7)
-    std::cout << "Задание 7" <<"\n";
+    std::cout << "╨Ч╨░╨┤╨░╨╜╨╕╨╡ 7" <<"\n";
     task7();
 #endif
 }
@@ -56,7 +56,7 @@ int ChekValue(T &Value,int L)
         {
             cin.clear();
             cin.ignore(32767,'\n');
-            cout<<" Некорректное значение\n";
+            cout<<" ╨Э╨╡╨║╨╛╤А╤А╨╡╨║╤В╨╜╨╛╨╡ ╨╖╨╜╨░╤З╨╡╨╜╨╕╨╡\n";
             i=-1;
         }
     }
