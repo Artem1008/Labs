59eaa13e376daba76da3bda1d1862501d9d3af56
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-07 12:56:40 +0700

    Update main.cpp

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -5,44 +5,44 @@ static int var{12};
 
 int main()
 {
-#if (TASK==1)
-    cout << "Задание 1" <<"\n";
+#if(TASK==1)
+    cout<<"Задание 1"<<"\n";
     task1();
 #endif
-#if (TASK==2)
-    cout << "Задание 2" <<"\n";
+#if(TASK==2)
+    cout<<"Задание 2"<<"\n";
     printChar();
     printChar('q');
     printChar('q','w');
     printChar('q','w','e');
     printChar('q','w','e','r');
 #endif
-#if (TASK==3)
-    cout << "Задание 3"<<"\n";
+#if(TASK==3)
+    cout<<"Задание 3"<<"\n";
     cout<<Initialization()<<'\n';
     int var=33;
     cout<<"Глобальная - "<<::var<<"; Локальная - "<<var;
 #endif
-#if (TASK==4)
-    std::cout << "Задание 4" <<"\n";
+#if(TASK==4)
+    std::cout<<"Задание 4"<<"\n";
     Animal zoo=goat;
     getAnimalName(zoo);
     printNumberOfLegs(zoo);
 #endif
-#if (TASK==5)
-    std::cout << "Задание 5" <<"\n";
-    std::cout <<"Введите 3 числа:\n";
+#if(TASK==5)
+    std::cout<<"Задание 5"<<"\n";
+    std::cout<<"Введите 3 числа:\n";
     int v[3];
     ChekValue(v,3);
-    std::cout << "Максимум : "<<SeekMax(v[0],v[1],v[2])<<'\n';
-    std::cout << "Минимум : "<<SeekMin(v[0],v[1],v[2])<<'\n';
+    std::cout<<"Максимум : "<<SeekMax(v[0],v[1],v[2])<<'\n';
+    std::cout<<"Минимум : "<<SeekMin(v[0],v[1],v[2])<<'\n';
 #endif
-#if (TASK==6)
+#if(TASK==6)
     std::cout << "Задание 6" <<"\n";
     task6();
 #endif
-#if (TASK==7)
-    std::cout << "Задание 7" <<"\n";
+#if(TASK==7)
+    std::cout<<"Задание 7"<<"\n";
     task7();
 #endif
 }
@@ -52,7 +52,7 @@ int ChekValue(T &Value,int L)
     for(int i=0;i<L;i++)
     {
         cin>>Value[i];
-        if (cin.fail())
+        if(cin.fail())
         {
             cin.clear();
             cin.ignore(32767,'\n');
