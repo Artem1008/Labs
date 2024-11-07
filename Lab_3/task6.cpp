78855954849d9f9cbf1904348f36d3585dc20819
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-07 12:58:40 +0700

    Update task6.cpp

--- Lab_3/task6.cpp
+++ Lab_3/task6.cpp
@@ -6,12 +6,12 @@ void task6()
     float y{1333.2324};
     double z{21474347.123};
     unsigned short k{12};
-    int i = static_cast<int>((x+y)*z+k) ;
+    int i=static_cast<int>((x+y)*z+k) ;
     std::cout<<"static_cast<int>((x+y)*z+k : "<<i<<'\n';
-    std::cout<<"k = x-y/z : "<<(k = x-y/z)<<'\n';
-    std::cout<<"z = x*y+z/x : "<<(z =(double)x*y+z/x)<<'\n';
-    unsigned int ui =(unsigned int) x/y+k;
+    std::cout<<"k = x-y/z : "<<(k=x-y/z)<<'\n';
+    std::cout<<"z = x*y+z/x : "<<(z=(double)x*y+z/x)<<'\n';
+    unsigned int ui=(unsigned int)x/y+k;
     std::cout<<"ui = x/y+k : "<<ui<<'\n';
-    short s = x%(int)(x/y);
+    short s=x%(int)(x/y);
     std::cout<<"s = x%(x/y) :"<<s;
 }
