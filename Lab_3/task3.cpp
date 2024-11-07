510a3ec4dd01728ed2faa026761f8ed13ce39225
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-07 12:57:35 +0700

    Update task3.cpp

--- Lab_3/task3.cpp
+++ Lab_3/task3.cpp
@@ -7,7 +7,7 @@ auto Initialization()->int
     bool bValue(1);
     char cValue{3};
     auto lValue=3147483647;
-    std::cout << typeid(iValue).name()<<" "<<typeid(bValue).name()<<" "<<typeid(cValue).name()<<" "<<typeid(lValue).name()<<'\n';
+    std::cout<<typeid(iValue).name()<<" "<<typeid(bValue).name()<<" "<<typeid(cValue).name()<<" "<<typeid(lValue).name()<<'\n';
     return 'a';
 }
 
