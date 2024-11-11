8485c280365fdcca02ee3fd0a102a19ac8fff91a
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-11 14:49:10 +0700

    Update main.cpp

--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -1,34 +1,32 @@
 #include "main.h"
-
 int main()
 {
-#if (TASK==1)
-    std::cout<< "Задание 1:"<<'\n';
+#if(TASK==1)
+    std::cout<<"Задание 1:"<<'\n';
     task1();
 #endif
-
-#if (TASK==2)
-    std::cout<< "Задание 2:"<<'\n';
+#if(TASK==2)
+    std::cout<<"Задание 2:"<<'\n';
     task2();
 #endif
-#if (TASK==3)
-    std::cout<< "Задание 3:"<<'\n';
+#if(TASK==3)
+    std::cout<<"Задание 3:"<<'\n';
     task3();
 #endif
 #if(TASK==4)
-    std::cout<< "Задание 4:"<<'\n';
+    std::cout<<"Задание 4:"<<'\n';
     task4();
 #endif
 #if(TASK==5)
-    std::cout<< "Задание 5:"<<'\n';
+    std::cout<<"Задание 5:"<<'\n';
     task5();
 #endif
 #if(TASK==6)
-    std::cout<< "Задание 6:"<<'\n';
+    std::cout<<"Задание 6:"<<'\n';
     task6();
 #endif
 #if(TASK==7)
-    std::cout<< "Задание 7:"<<'\n';
+    std::cout<<"Задание 7:"<<'\n';
     task7();
 #endif
 }
