4cacd7154670f5938feedc07bbae2fe824a0ccff
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-11 14:48:40 +0700

    Update main.cpp

--- Lab_4/main.cpp
+++ Lab_4/main.cpp
@@ -3,32 +3,32 @@
 int main()
 {
 #if (TASK==1)
-    std::cout<< "‡ ¤ ­¨¥ 1:"<<'\n';
+    std::cout<< "Ð—Ð°Ð´Ð°Ð½Ð¸Ðµ 1:"<<'\n';
     task1();
 #endif
 
 #if (TASK==2)
-    std::cout<< "‡ ¤ ­¨¥ 2:"<<'\n';
+    std::cout<< "Ð—Ð°Ð´Ð°Ð½Ð¸Ðµ 2:"<<'\n';
     task2();
 #endif
 #if (TASK==3)
-    std::cout<< "‡ ¤ ­¨¥ 3:"<<'\n';
+    std::cout<< "Ð—Ð°Ð´Ð°Ð½Ð¸Ðµ 3:"<<'\n';
     task3();
 #endif
 #if(TASK==4)
-    std::cout<< "‡ ¤ ­¨¥ 4:"<<'\n';
+    std::cout<< "Ð—Ð°Ð´Ð°Ð½Ð¸Ðµ 4:"<<'\n';
     task4();
 #endif
 #if(TASK==5)
-    std::cout<< "‡ ¤ ­¨¥ 5:"<<'\n';
+    std::cout<< "Ð—Ð°Ð´Ð°Ð½Ð¸Ðµ 5:"<<'\n';
     task5();
 #endif
 #if(TASK==6)
-    std::cout<< "‡ ¤ ­¨¥ 6:"<<'\n';
+    std::cout<< "Ð—Ð°Ð´Ð°Ð½Ð¸Ðµ 6:"<<'\n';
     task6();
 #endif
 #if(TASK==7)
-    std::cout<< "‡ ¤ ­¨¥ 7:"<<'\n';
+    std::cout<< "Ð—Ð°Ð´Ð°Ð½Ð¸Ðµ 7:"<<'\n';
     task7();
 #endif
 }
