37f95c725dbb8df31ede523b988729785cb3ad85
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-12 20:04:17 +0700

    Update task7.cpp

--- Lab_4/task7.cpp
+++ Lab_4/task7.cpp
@@ -2,5 +2,22 @@
 
 int task7()
 {
-
+    std::string Engine="UVW";
+    int size=Engine.length();
+   for(int a1=0; a1<size;a1++)
+    {
+        for(int a2=0; a2<size;a2++)
+        {
+            if(a1!=a2)
+            {
+                for(int a3=0; a3<size;a3++)
+                {
+                    if(a2!=a3&&a1!=a3)
+                    {
+                        printf("%c %c %c\n",Engine[a1],Engine[a2],Engine[a3]);
+                    }
+                }
+            }
+        }
+    }
 }
