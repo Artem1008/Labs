94fd31a0f5f543cf67e31ebc9c0d102940adf790
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-14 16:22:06 +0700

    Update main.h

--- Dop_4/main.h
+++ Dop_4/main.h
@@ -22,43 +22,68 @@ void InputSize(int & size);
 template<typename T>
 int ChekValue(T &Value);
 /**
-* @brief Функция Создания массива
+* @brief Функция выделения памяти
 */
 template<typename T>
-void CreateArray(T* &pArr,int size);
+void AccumMem(T* &pArr,int size);
+/**
+* @brief Функция очистки памяти
+*/
+template<typename T>
+void FreeMem(T* &pArr);
+/**
+* @brief Функция Пирамидальной сортировки
+*/
+template<typename T>
+void PyramidSort(T*,int);
+/**
+* @brief Функция сборки убывающей кучи
+*/
+template<typename T>
+void heapify(T* , int, int);
+/**
+* @brief Функция проверки элементов
+*/
+template<typename T>
+void ChekPyramidSort(T*,int,int);
+/**
+* @brief Функция сортировки методом перебора
+*/
+template<typename T>
+void EnumerationSort(T*,int);
+/**
+* @brief Функция поиска наименьшего элемента
+*/
+template<typename T>
+int LookLittle(T* pArr,int start, int size);
+/**
+* @brief Функция сортировки методом пузырька
+*/
+template<typename T>
+void BubbleSort(T*,int);
+/**
+* @brief Функция запуска юыстрой сортировки
+*/
+template<typename T>
+void FirsStart(T* pArr,int size);
 /**
 * @brief Функция быстрой сортировки
 */
 template<typename T>
-void QuickSort(T* pArr,int begin, int end);
+void Quicksort(T* pArr,int begin, int end);
 /**
 * @brief Функция разбиения массива для быстрой сортировки
 */
 template<typename T>
 int Partition(T* pArr,int begin, int end);
 /**
-* @brief Функция Пирамидальной сортировки
+* @brief Функция генерации случаных чисел
 */
 template<typename T>
-void PyramidSort(T* pArr,int begin, int end);
+void GeneralValue(T* &pArr,int size);
 /**
-* @brief Функция сортировки методом перебора
+* @brief Функция печати массива
 */
 template<typename T>
-void EnumerationSort(T* pArr,int begin, int end);
-/**
-* @brief Функция сортировки методом пузырька
-*/
-template<typename T>
-void BubbleSort(T* pArr,int begin, int end);
-/**
-* @brief Функция сортировки методом пузырька
-*/
-template<typename T>
-void HaorSort(T* pArr,int begin, int end);
-/**
-* @brief Функция выбора указателя на функцию
-*/
-template<typename T>
-void(*select(int))(T* ,int,int);
+void PrintArray(T* pArr,int size);
 #endif // MAIN_H
