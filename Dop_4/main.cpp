cddd61b2388eb9989ec8e407e1e9b669c3167d35
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-14 16:21:55 +0700

    Update main.cpp

--- Dop_4/main.cpp
+++ Dop_4/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <ctime>
+#include <cmath>
 #include "main.h"
 
 template<typename T>
-void ((*actions[])(T*,int,int)){PyramidSort, EnumerationSort, BubbleSort, HaorSort};
+void(*select(int))(T*,int);
+template<typename T>
+void ((*actions[])(T*,int)){EnumerationSort,BubbleSort,FirsStart,PyramidSort};
 int main() {
     int type=0;
     int size=0;
     int typeSort=0;
-
     while(true)
     {
         InputType(type);
@@ -18,88 +21,47 @@ int main() {
             if(type==1)
             {
                 char* pArr;
-                void(*action)(char*,int, int );
-                CreateArray(pArr,size);
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<"\n";
-                action=select(1);
-                if(action) action(pArr,0,size-1);
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<'\n';
+                void(*action)(char*,int) {select<char>(typeSort)};
+                AccumMem(pArr,size);
+                if(action) action(pArr,size);
+                PrintArray(pArr,size);
+                FreeMem(pArr);
             }
             else if(type==2)
             {
                 short* pArr;
-                void(*action)(short*,int, int );
-                CreateArray(pArr,size);
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<"\n";
-
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<'\n';
+                void(*action)(short*,int) {select<short>(typeSort)};
+                AccumMem(pArr,size);
+                if(action) action(pArr,size);
+                PrintArray(pArr,size);
+                FreeMem(pArr);
             }
             else if(type==3)
             {
                 int* pArr;
-                void(*action)(int*,int, int );
-                CreateArray(pArr,size);
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<"\n";
-
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<'\n';
+                void(*action)(int*,int) {select<int>(typeSort)};
+                AccumMem(pArr,size);
+                if(action) action(pArr,size);
+                PrintArray(pArr,size);
+                FreeMem(pArr);
             }
             else if(type==4)
             {
                 float* pArr;
-                void(*action)(float*,int, int );
-                CreateArray(pArr,size);
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<"\n";
-
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<'\n';
+                void(*action)(float*,int) {select<float>(typeSort)};
+                AccumMem(pArr,size);
+                if(action) action(pArr,size);
+                PrintArray(pArr,size);
+                FreeMem(pArr);
             }
             else if(type==5)
             {
                 double* pArr;
-                void(*action)(double*,int, int );
-                CreateArray(pArr,size);
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<"\n";
-
-                for(int i=0;size>i;i++)
-                {
-                    std::cout<<pArr[i]<<" ";
-                }
-                std::cout<<'\n';
+                void(*action)(double*,int) {select<double>(typeSort)};
+                AccumMem(pArr,size);
+                if(action) action(pArr,size);
+                PrintArray(pArr,size);
+                FreeMem(pArr);
             }
             else if (type==9)
             {
@@ -116,12 +78,10 @@ int main() {
 
 }
 template<typename T>
-void(*select(int choice))(T* ,int,int)
+void(*select(int choice))(T*,int)
 {
-    if (choice >0 && choice<5)
-    return actions[choice - 1];
-    else
-    return nullptr;
+    if (choice >0 && choice<5) return actions<T>[choice - 1];
+    else return nullptr;
 }
 void InputType(int & type)
 {
@@ -133,8 +93,9 @@ void InputType(int & type)
     printf("5. double\n");
     printf("9. Выход\n");
     ChekValue(type);
+    if (type==9) exit(0);
 }
-void InputSort(int & type)
+void InputSort(int & typeSort)
 {
     printf("Введите Метод сортировки:\n");
     printf("1. Перебор\n");
@@ -142,7 +103,8 @@ void InputSort(int & type)
     printf("3. Хаора\n");
     printf("4. Пирамидальной\n");
     printf("9. Выход\n");
-    ChekValue(type);
+    ChekValue(typeSort);
+    if (typeSort==9) exit(0);
 }
 void InputSize(int & size)
 {
@@ -150,35 +112,109 @@ void InputSize(int & size)
     ChekValue(size);
 }
 template<typename T>
-void CreateArray(T* &pArr,int size)
+void AccumMem(T* &pArr,int size)
 {
-    pArr=new T[size];
-    srand(7777);
-    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX)+1.0);
-    for (int i=0; i<size; i++)
+    pArr=new (std::nothrow)T[size];
+    GeneralValue(pArr,size);
+}
+template<typename T>
+void FreeMem(T* &pArr)
+{
+    if (pArr) delete[] pArr;
+    pArr = nullptr;
+}
+template<typename T>
+void GeneralValue(T* &pArr,int size)
+{
+   double maxValue{std::pow(255, sizeof(*pArr))};
+    srand(time(0));
+    for (int i = 0; i < size; ++i) {
+        *(pArr + i) = static_cast<T>(1 + rand() % (int)maxValue);
+        std::cout<<*(pArr + i)<<" ";
+    }
+    std::cout<<'\n';
+}
+template<typename T>
+void PrintArray(T* pArr,int size)
+{
+    for(int i=0;size>i;i++)
     {
-        pArr[i]=static_cast<int>(rand() * fraction * (MAXVALUE-MINVALUE+1) +MINVALUE);
+        std::cout<<pArr[i]<<" ";
+    }
+    std::cout<<'\n';
+}
+template<typename T>
+void BubbleSort(T* pArr,int size)
+{
+    printf("Сортировка методом пузырька\n");
+    while(size--) {
+        bool swapped = false;
+        for(int i = 0; i < size; i++)
+        {
+            if(pArr[i] > pArr[i + 1])
+            {
+                std::swap(pArr[i], pArr[i + 1]);
+                swapped = true;
+            }
+        }
+        if(swapped == false)
+            break;
     }
 }
 template<typename T>
-void BubbleSort(T* pArr,int begin, int end)
+void heapify(T* pArr, int n, int i)
 {
-std::cout<<"BubbleSort";
+    int largest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+
+    if (l < n && pArr[l] > pArr[largest])
+        largest = l;
+
+    if (r < n && pArr[r] > pArr[largest])
+        largest = r;
+
+    if (largest != i)
+    {
+        std::swap(pArr[i], pArr[largest]);
+        heapify(pArr, n, largest);
+    }
+}
+
+template<typename T>
+void PyramidSort(T* pArr, int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(pArr, n, i);
+    for (int i=n-1; i>=0; i--)
+    {
+        std::swap(pArr[0], pArr[i]);
+        heapify(pArr, i, 0);
+    }
 }
 template<typename T>
-void PyramidSort(T* pArr,int begin, int end)
+void EnumerationSort(T* pArr,int size)
 {
-    std::cout<<"PyramidSort";
+    for(int i = 0; i < size; i++)
+    {
+        int LittlePosition = LookLittle(pArr, i, size);
+        std::swap(pArr[i], pArr[LittlePosition]);
+    }
 }
 template<typename T>
-void EnumerationSort(T* pArr,int begin, int end)
+int LookLittle(T* pArr,int start, int size)
 {
-    std::cout<<"EnumerationSort";
+    int LittlePosition=size-1;
+    for(int i=size-1;i<start;i--)
+    {
+        if(pArr[LittlePosition]>pArr[i]) LittlePosition=i;
+    }
+    return LittlePosition;
 }
 template<typename T>
-void HaorSort(T* pArr,int begin, int end)
+void FirsStart(T* pArr,int size)
 {
-    std::cout<<"HaorSort";
+    Quicksort(pArr,0,size-1);
 }
 template<typename T>
 void Quicksort(T* pArr,int begin, int end)
