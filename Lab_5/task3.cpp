574a3a90a346db6f940a9bd150abcf69b6b94c69
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-26 11:42:22 +0700

    Update task3.cpp

--- Lab_5/task3.cpp
+++ Lab_5/task3.cpp
@@ -1,17 +1,6 @@
 #include "main.h"
 
-void task3_1()
+void task3()
 {
-    std::string text="Александр             Македонский герой,\tно зачем                же стулья\t                       ломать";
 
-    for(int i=0;i<=(int)text.length();i++)
-    {
-        if (text[i]=='\t') text[i]=' ';
-        if ((text[i]==' ')&&(text[i-1]==' '))
-        {
-            i--;
-            text.erase(i,1);
-        }
-    }
-    std::cout<<text;
 }
