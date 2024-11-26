314ee28ace1f2a891ac2b7e3c942918057e2cf5f
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-26 18:10:53 +0700

    Update task5.cpp

--- Lab_5/task5.cpp
+++ Lab_5/task5.cpp
@@ -4,7 +4,7 @@ void task5()
 {
     std::string text="Александр             Македонский герой,\tно зачем                же стулья\t                       ломать";
 
-     for(int i=0;i<=(int)text.length();i++)
+     for(int i=1;i<=(int)text.length();i++)
      {
          if (text[i]=='\t') text[i]=' ';
          if ((text[i]==' ')&&(text[i-1]==' '))
