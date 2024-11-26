5d4676df1266a951e7e2b8cf73cdfc75f014736b
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-26 11:42:43 +0700

    Update task5.cpp

--- Lab_5/task5.cpp
+++ Lab_5/task5.cpp
@@ -1,17 +1,17 @@
-    std::string text="Александр             Македонский герой,\tно зачем                же стулья\t                       ломать";
-
-    for(int i=0;i<=(int)text.length();i++)
-    {
-        if (text[i]=='\t') text[i]=' ';
-        if ((text[i]==' ')&&(text[i-1]==' '))
-        {
-            i--;
-            text.erase(i,1);
-        }
-    }
-    std::cout<<text;#include "main.h"
+#include "main.h"
 
 void task5()
 {
+    std::string text="Александр             Македонский герой,\tно зачем                же стулья\t                       ломать";
 
+     for(int i=0;i<=(int)text.length();i++)
+     {
+         if (text[i]=='\t') text[i]=' ';
+         if ((text[i]==' ')&&(text[i-1]==' '))
+         {
+             i--;
+             text.erase(i,1);
+         }
+     }
+     std::cout<<text;
 }
