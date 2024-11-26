49e171ffafa826ef0f04e467573c3d323be307e5
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-26 18:09:28 +0700

    Update task4.cpp

--- Lab_5/task4.cpp
+++ Lab_5/task4.cpp
@@ -1,9 +1,25 @@
 #include "main.h"
-#include <cstring>
-
-
 void task4()
 {
-
+    std::string str="AAAABCCCCCDDDDE";
+    int countABC=0;
+    char ABC;
+    std::string rezult;
+    for(int i=1;i<=(int)str.length();i++)
+    {
+        if ((str[i]==str[i-1]))
+        {
+            countABC++;
+        }
+        else
+        {
+            if(countABC>0)
+            {
+                rezult=rezult+IntToString(countABC+1);
+                countABC=0;
+            }
+            rezult=rezult+str[i-1];
+        }
+    }
+    printf("%s",rezult.c_str());
 }
-
