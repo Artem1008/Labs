54a8bdde81458ca807d6c85a6eeffd5a92bf6add
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-11-27 18:59:06 +0700

    Update main.h

--- Dop_5/main.h
+++ Dop_5/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <iostream>
-
+/**
+* @brief Функция для первого задания
+*/
+void task1();
+/**
+* @brief Функция для второго задания
+*/
+void task2();
 #endif // MAIN_H
