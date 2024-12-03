5726841b4552ed1375d6b59afe3363ca17ea6a0a
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-12-03 13:38:12 +0700

    Update bruteforce.h

--- Lab_6/bruteforce.h
+++ Lab_6/bruteforce.h
@@ -1,5 +1,6 @@
 #ifndef BRUTEFORCE_H
 #define BRUTEFORCE_H
+#include <QDebug>
 #include <string>
 extern std::string pass;
 class Bruteforce
@@ -8,11 +9,11 @@ private:
     bool flagNumber;
     bool flagCapital;
     bool flagSymbols;
-    char *Capital=new char[27];
-    char  *Letters=new char[27];
-    char *Number=new char[11];
-    char *Symbols=new char[18];
-
+    int lengthPass;
+    const char *Capital=new char[27]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
+    const char *Letters=new char[27]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\0'};
+    const char *Number=new char[11]{'0','1','2','3','4','5','6','7','8','9','\0'};
+    const char * Symbols=new char[15]{'!',';','%',':','?','*','(',')','~','.',',','/','|','\\','\0'};
 public:
     bool getFlagNumber(){return flagNumber;}
     void setFlagNumber(bool _flag){flagNumber= _flag;}
@@ -20,6 +21,8 @@ public:
     void setFlagCapital(bool _flag){flagCapital= _flag;}
     bool getFlagSymbols(){return flagSymbols;}
     void setFlagSymbols(bool _flag){flagSymbols= _flag;}
+    int getLengthPass(){return lengthPass;}
+    void setLengthPass(int _lengthPass){lengthPass= _lengthPass;}
     Bruteforce();
     std::string RunFind();
 };
