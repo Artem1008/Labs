dd50e8c2514e8df155086eaececd89e9003aa552
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-12-07 17:49:06 +0700

    Update task4.cpp

--- Lab_5/task4.cpp
+++ Lab_5/task4.cpp
@@ -3,11 +3,10 @@ void task4()
 {
     std::string str="AAAABCCCCCDDDDE";
     int countABC=0;
-    char ABC;
     std::string rezult;
     for(int i=1;i<=(int)str.length();i++)
     {
-        if ((str[i]==str[i-1]))
+        if (str[i]==str[i-1])
         {
             countABC++;
         }
