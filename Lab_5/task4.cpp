bc3e688db62f9d6b6db33729d6db9187bae150cc
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-12-07 18:33:11 +0700

    Update task4.cpp

--- Lab_5/task4.cpp
+++ Lab_5/task4.cpp
@@ -1,9 +1,9 @@
 #include "main.h"
-void task4()
+
+std::string Pack(std::string str)
 {
-    std::string str="AAAABCCCCCDDDDE";
-    int countABC=0;
     std::string rezult;
+    int countABC=0;
     for(int i=1;i<=(int)str.length();i++)
     {
         if (str[i]==str[i-1])
@@ -20,5 +20,39 @@ void task4()
             rezult=rezult+str[i-1];
         }
     }
-    printf("%s",rezult.c_str());
+    return rezult;
 }
+
+std::string Unpack(std::string str)
+{
+    std::string rezult;
+    std::string sCount;
+    for(int i=0;i<(int)str.length();i++)
+    {
+        while (isdigit(str[i]))
+        {
+            sCount += str[i];
+        }
+        if(sCount!="")
+        {
+            for(int i=0;i<stod(sCount);i++)
+            {
+                rezult+=str[i];
+            }
+            sCount="";
+        }
+        else
+        {
+            rezult+=str[i];
+        }
+    }
+    return rezult;
+}
+void task4()
+{
+    std::string str="AAAABCCCCCDDDDE";
+    printf("Исходная строка: %s\n",str.c_str());
+    printf("После упоковки: %s\n",Pack(str).c_str());
+    printf("После распоковки: %s\n",Unpack(str).c_str());
+}
+
