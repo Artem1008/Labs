7f9255e0b806949a329a99e7c15772d8f52bb6ce
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-12-14 16:04:31 +0700

    Update depomodel.h

--- Lab_6/depomodel.h
+++ Lab_6/depomodel.h
@@ -1,6 +1,7 @@
 #ifndef LISTMODELFLIGHT_H
 #define LISTMODELFLIGHT_H
 #include <QStandardItemModel>
+#include <QDebug>
 #include "flight.h"
 #include "dispatcher.h"
 #include "driver.h"
@@ -11,18 +12,16 @@ class TreeModel : public QStandardItemModel
     Q_OBJECT
 
 public:
-    explicit TreeModel(Dispatcher& _disp,QVector<Driver>& _drivers, QVector<Car>&_cars, QObject *parent);
+    explicit TreeModel(Dispatcher& _disp,QVector<Driver>& _drivers, QVector<Car>&_cars,QVector<Flight> & _flight, QObject *parent);
     void initialize();
     void SetFlight(Flight _value);
+   QVariant data(const QModelIndex& index, int nRole) const;
 private:
     Dispatcher* muDisp;
     QVector<Driver>* myDrivers;
     QVector<Car>* myCar;
-    QVector<QPair<QString, Flight>>myFlight{
-        {"Заявка 1", {1,"Томск","Новосибирск" }},
-        {"Заявка 2", {2,"Новосибирск","Кемерово" }},
-        {"Заявка 3", {3,"Кемерово","Топки" }},
-    };
+    QVector<Flight>* myFlight;
 };
 
 #endif // LISTMODELFLIGHT_H
+
