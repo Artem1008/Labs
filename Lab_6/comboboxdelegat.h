683ec54b06334d80fcd1a9f76eb4a076b9d262e9
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-12-16 11:44:10 +0700

    Update comboboxdelegat.h

--- Lab_6/comboboxdelegat.h
+++ Lab_6/comboboxdelegat.h
@@ -10,9 +10,9 @@ class ComboBoxDelegat:public QItemDelegate
 {
     Q_OBJECT
  private:
-    QVector<Driver> myDrivers;
+    QMap<QString,Driver> myDrivers;
 public:
-    ComboBoxDelegat(QVector<Driver>& _drivers,QObject *parent = 0);
+    ComboBoxDelegat(QMap<QString,Driver>& _drivers,QObject *parent = 0);
     QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,const QModelIndex &index) const;
     void setEditorData(QWidget *editor, const QModelIndex &index) const;
     void setModelData(QWidget *editor, QAbstractItemModel *model,const QModelIndex &index) const;
