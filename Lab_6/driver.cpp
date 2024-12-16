c0529e1b8df7e05a5c83baac3b1c526dc9696d8a
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2024-12-16 11:47:07 +0700

    Update driver.cpp

--- Lab_6/driver.cpp
+++ Lab_6/driver.cpp
@@ -3,3 +3,9 @@ void Driver::UpdateStatusCar(StatusCar _statuscar)
 {
     mycar->setStatus(_statuscar);
 }
+
+QString Driver::GetNameCar() const
+{
+    if (mycar!=nullptr) return mycar->getName();
+    else return "не назначена";
+}
