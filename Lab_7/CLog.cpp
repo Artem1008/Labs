f3d35a22c8e49439a58b3866710bd678a647224f
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-01-27 11:56:35 +0700

    Update CLog.cpp

--- Lab_7/CLog.cpp
+++ Lab_7/CLog.cpp
@@ -1,13 +1,24 @@
 #include "CLog.h"
-
+int CLog::Level;
 CLog::CLog()
 {
 
 }
 void CLog::PrintMessage(int priority,const char* str)
 {
+ if (CheckLevel(priority))
+    {
     const std::time_t MessageTime = std::time(nullptr);
     char buf[64];
     strftime(buf, sizeof buf, "%c", std::localtime(&MessageTime));
     printf("%s  %s:  %s\n",buf,arr(priority),str);
+    }
+}
+void CLog::SetLevel(int _level)
+{
+    Level=_level;
+}
+bool CLog::CheckLevel(int _level)
+{
+    return (_level&Level);
 }
