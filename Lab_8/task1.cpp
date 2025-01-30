73d5eb38faaac9f5cd5d61dd072ca8ef6db7498a
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-01-30 10:35:41 +0700

    Update task1.cpp

--- Lab_8/task1.cpp
+++ Lab_8/task1.cpp
@@ -1,21 +1,9 @@
-#include <iostream>
 #include <vector>
-#include <ctime>
 #include <map>
+#include <myfuct.h>
 
-unsigned long rand32(void)
-{
-    static unsigned long long rand = time(NULL);
-    rand *= 134775813;
-    rand += 1;
-    return (unsigned long) (rand >> 32);
-}
 template<typename T>
 int ChekVector(const std::vector<T>&);
-template<typename T>
-std::ostream& operator << (std::ostream& os, const std::vector<T>&);
-template<typename T>
-int ChekValue(T &);
 int task1()
 {
     int N;
@@ -26,27 +14,11 @@ int task1()
     }
     std::vector<int> arr;
     printf("Содержимое контейнера:\n");
-    for(int i=0;i<N;i++)
-    {
-        arr.push_back(rand32());
-        //Используйте перегрузку ввода и вывода (cout, cin).
-        std::cout<<arr;
-        /*
-        printf("%d ",arr[i]);
-        if (((i+1)%10==0)) printf("\n");
-        */
-    }
+    CreateCont(arr,N);
+    std::cout<<arr;
     printf("Уникальных чисел:");
     std::cout<<ChekVector(arr);
       return 1;
-}
-template<typename T>
-std::ostream& operator << (std::ostream& os, const std::vector<T>& vec)
-{
-   os.width(12);
-   os<<vec.back();
-   if (vec.size()%10==0) os<<'\n';
-   return os;
 }
 template<typename T>
 int ChekVector(const std::vector<T>& vec)
@@ -64,24 +36,5 @@ int ChekVector(const std::vector<T>& vec)
     }
     return Count;
 }
-template<typename T>
-int ChekValue(T &Value)
-{
-    int temp;
-    int count=5;
-    for(;count>=0;count--)
-    {
-        scanf("%d", &temp);
-        if ((temp>1000)||(temp<0))
-        {
-            printf("%s","Вы ввели некоректное значение\n");
-        }
-        else
-        {
-            Value=temp;
-            return 1;
-        }
-    }
-    std::cout<<"Привышено число попыток ввода данных\n";
-    return 0;
-}
+
+
