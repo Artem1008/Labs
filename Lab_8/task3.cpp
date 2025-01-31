4e7ed3c1bb04b38d2be3229afafa8603ff1d4356
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-01-31 19:38:51 +0700

    Update task3.cpp

--- Lab_8/task3.cpp
+++ Lab_8/task3.cpp
@@ -1,24 +1,56 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <conio.h>
 
 #define KEY_UP 72
 #define KEY_DOWN 80
 #define BACKSPACE 8
+
+class Bytes
+{
+private:
+    std::map<std::string,unsigned int> tableConvert;
+    unsigned long long value;
+public:
+    Bytes():value(0)
+    {
+        tableConvert.insert(std::make_pair("byte", 1));
+        tableConvert.insert(std::make_pair("Kbyte", 1024));
+        tableConvert.insert(std::make_pair("MiB", 1048576));
+        tableConvert.insert(std::make_pair("Мbyte", 1048576));
+        tableConvert.insert(std::make_pair("Gbyte", 1073741824));
+    }
+    void addByte(unsigned long long _value,std::string unit)
+    {
+
+        value= ((value/tableConvert[unit])*10+_value)*tableConvert[unit];
+    }
+    void subByte(std::string unit)
+    {
+        value=((value/tableConvert[unit])/10)*tableConvert[unit];
+    }
+    unsigned long long getByte(std::string unit)
+    {
+        return value/tableConvert[unit];
+    }
+
+};
+
 void ClearStringConsole(int value,std::string str);
 void task3()
 {
     //если работает с STL будет на контейнерах
     //char units[][10]={"Gbyte","Мbyte","Kbyte","byte","MiB"};
-    std::vector<std::string> units ={"Gbyte","Мbyte","Kbyte","byte","MiB"};
+    std::vector<std::string> units ={"byte","Kbyte","Мbyte","MiB","Gbyte"};
     std::vector<std::string>::iterator itunits=units.begin();
     char c=0;
-    int value=0;
+    Bytes value;
     printf("Введите число и стрелками вверх вниз выберите ед.изм.\n");
     while(c != 13)
     {
         c=getch();
-        ClearStringConsole(value,(*itunits));
+        ClearStringConsole(value.getByte(*itunits),(*itunits));
         if (c == KEY_UP)
         {
             if(itunits==units.end())
@@ -29,23 +61,23 @@ void task3()
             {
                 itunits++;
             }
-            printf("\r%d %s",value,(*itunits).c_str());
+           printf("\r%lld %s",value.getByte(*itunits),(*itunits).c_str());
         }
         if (c == KEY_DOWN)
         {
             if(itunits==units.begin()) itunits=units.end();
             else itunits--;
-            printf("\r%d %s",value,(*itunits).c_str());
+            printf("\r%lld %s",value.getByte(*itunits),(*itunits).c_str());
         }
         if((c>='0')&&c<='9')
         {
-            value=value*10+(c-48);
-            printf("\r%d %s",value,(*itunits).c_str());
+            value.addByte((c-48),*itunits);
+            printf("\r%lld %s",value.getByte(*itunits),(*itunits).c_str());
         }
         if(c==BACKSPACE)
         {
-            if (value>0) value/=10;
-            printf("\r%d %s",value,(*itunits).c_str());
+            if (value.getByte(*itunits)>0) value.subByte(*itunits);
+           printf("\r%lld %s",value.getByte(*itunits),(*itunits).c_str());
         }
     }
 }
