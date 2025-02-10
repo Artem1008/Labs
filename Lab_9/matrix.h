0238b4918451d2eb603f3102ed8a8be4358c1fca
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-02-10 14:51:20 +0700

    Update matrix.h

--- Lab_9/matrix.h
+++ Lab_9/matrix.h
@@ -17,14 +17,26 @@ public:
     Matrix<T>& multiply(int x);
     Matrix<T>& add(Matrix<T> m);
     Matrix<T>& sub(Matrix<T> m);
-    Matrix<T> operator+(const Matrix<T> m) const;
-    Matrix<T> operator-(const Matrix<T> m) const;
-    Matrix<T> operator*(const Matrix<T> m) const;
-    Matrix<T>& operator=(const Matrix<T>& m) const;
-    template<class T2>
+    Matrix<T> operator+(const Matrix<T>& m) const;
+    Matrix<T> operator-(const Matrix<T>& m) const;
+    Matrix<T> operator*(const Matrix<T>& m) const;
+    Matrix<T>& operator=(const Matrix<T>& m);
+    template<typename T2>
+    friend Matrix<T>& operator*(Matrix<T>& refmatrix,T2 val)
+    {
+        Matrix<T>* temp=new Matrix<T>(refmatrix.rows,refmatrix.columns);
+        for(int i=0;i<refmatrix.rows;++i)
+        {
+            for (int j = 0; j<refmatrix.columns; ++j)
+            {
+                 temp->data[i][ j]=refmatrix.data[i][ j]*val;
+            }
+        }
+        return *temp;
+    }
+    template<typename T2>
     friend std::ostream& operator << (std::ostream& os, const Matrix<T2>& refmatrix);
 };
 
 #include <matrix.inl>
-
 #endif // MATRIX_H
