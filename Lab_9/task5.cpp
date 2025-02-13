4469dd44c1d41e63277f7e376943c3ddf92d2e5c
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-02-13 15:51:07 +0700

    Update task5.cpp

--- Lab_9/task5.cpp
+++ Lab_9/task5.cpp
@@ -1,4 +1,17 @@
+#include <fstream>
+
 int task5()
 {
-
+    std::ofstream file;
+    file.open("Cat.txt");
+   file<<" _ "<<'\n';
+    file<<"( \\"<<'\n';
+    file<<" ) ) "<<'\n';
+    file<<"( (  .-''''-.  A.-.A"<<'\n';
+    file<<" \\ \\/        \\/ , , \\"<<'\n';
+    file<<"  \\  \\       =;  t  /="<<'\n';
+    file<<"   \\  |''''.   ',--'"<<'\n';
+    file<<"   / //    |  ||"<<'\n';
+    file<<"  /_,))    |_,))"<<'\n';
+    return 1;
 }
