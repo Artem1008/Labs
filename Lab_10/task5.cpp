ac23da0b2b1c1f0f88d04522700063728983090c
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-03-14 18:23:56 +0700

    Update task5.cpp

--- Lab_10/task5.cpp
+++ Lab_10/task5.cpp
@@ -1,14 +1,13 @@
 #include <fstream>
 #include <vector>
 #include <functional>
-#include <iostream>
 
 auto defaultValidString=[](const std::string& str)
 {
     for(const char &var:str)
     {
         if (var==' '||var=='\n'||var=='\t') {}
-       else return true;
+        else return true;
     }
     return false;
 };
@@ -16,15 +15,34 @@ auto defaultValidId=[](int index, const std::vector<std::string>& doc)
 {
     return index <(int)doc.size();
 };
+void reversrecurssumm(int index, std::vector<std::string>& doc,std::string& str)
+{
+    std::string temp;
+    std::string::reverse_iterator itBeginStr=doc[index-1].rbegin();
+    for(;itBeginStr!=doc[index-1].rend();++itBeginStr)
+    {
+        if (*itBeginStr=='.'||*itBeginStr==';')
+        {
+            temp.assign(doc[index-1].rbegin()+1,itBeginStr);
+            std::reverse(temp.begin(),temp.end());
+            str.push_back(' ');
+            str=temp+" "+str;
+            return;
+        }
+    }
+    str=doc[index-1]+str;
+    if(index-1 >=0)
+        reversrecurssumm(index-1,doc,str);
+}
 void recurssumm(int index, std::vector<std::string>& doc,std::string& str)
 {
     for(const char &var:doc[index])
     {
-        if (var=='.') return ;
+        if (var=='.'||var==';') return ;
         str.push_back(var);
     }
     if(index+1 <(int)doc.size())
-    recurssumm(index+1,doc,str);
+        recurssumm(index+1,doc,str);
 }
 class Guessing
 {
@@ -54,25 +72,15 @@ public:
         std::string rezult;
         if (!ValidId(index,doc))
         {
-            return "строка с данных номером не существует, попробуйте снова\n";
+            return "Неверный номер строки. Попробуйте снова\n";
         }
         if (!ValidString(doc[index]))
         {
-            return "данная строка пустая\n";
+            return "Полученная строка не содержит предсказание. Попробуйте другой номер\n";
         }
-        std::string::reverse_iterator itBeginStr=doc[index-1].rbegin();
-        if(*itBeginStr=='.') rezult= "";
+        if(*doc[index-1].rbegin()=='.'||*doc[index-1].rbegin()==';') rezult= "";
         else{
-            for(;itBeginStr!=doc[index-1].rend();++itBeginStr)
-            {
-                if (*itBeginStr=='.')
-                {
-                    rezult.assign(doc[index-1].rbegin()+1,itBeginStr);
-                    std::reverse(rezult.begin(),rezult.end());
-                    break;
-                }
-            }
-            rezult.push_back(' ');
+            reversrecurssumm(index,doc,rezult);
         }
         recurssumm(index,doc,rezult);
         return rezult;
@@ -86,10 +94,17 @@ public:
         ValidId=func;
     }
 };
+
 int task5()
 {
-    setlocale(LC_ALL,"Russian");
-    Guessing text("divination.txt");
-    std::cout<<text.prediction(3);
+    int index=0;
+    printf("Загадайте желание или задайте интересующий вопрос и введите номер строки: \n");
+    while(index!=666)
+    {
+        scanf("%d",&index);
+        setlocale(LC_ALL,"Russian");
+        Guessing text("D:\\ProjectQt\\Lab_10\\divination.txt");
+        printf("%s\n",text.prediction(index-1).c_str());
+    }
     return 1;
 }
