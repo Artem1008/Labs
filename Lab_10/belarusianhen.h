19bd554b490e1983108d6d0704816c50a6658854
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-03-14 18:47:37 +0700

    Update belarusianhen.h

--- Lab_10/belarusianhen.h
+++ Lab_10/belarusianhen.h
@@ -6,8 +6,8 @@ class BelarusianHen: public Hen
 {
     public:
     std::string country = "Белорусия";
-    int getCountOfEggsPerMonth(){ return 20;}
-    std::string getDescription();
+    int getCountOfEggsPerMonth() override { return 20;}
+    std::string getDescription() override ;
     virtual ~BelarusianHen(){}
 };
 
