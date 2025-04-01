3a7a9b0e00fde07dc9875ad44f04c631e814e482
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-04-01 13:15:22 +0700

    Update task5.cpp

--- Lab_11/task5.cpp
+++ Lab_11/task5.cpp
@@ -3,22 +3,32 @@
 class Point {
     int m_x, m_y;
 public:
-    Point() // конструктор без параметров
+    Point() // ╨║╨╛╨╜╤Б╤В╤А╤Г╨║╤В╨╛╤А ╨▒╨╡╨╖ ╨┐╨░╤А╨░╨╝╨╡╤В╤А╨╛╨▓
         : m_x(0), m_y(0){}
-    Point(int x, int y) // конструктор с параметрами
+    Point(int x, int y) // ╨║╨╛╨╜╤Б╤В╤А╤Г╨║╤В╨╛╤А ╤Б ╨┐╨░╤А╨░╨╝╨╡╤В╤А╨░╨╝╨╕
         : m_x(x), m_y(y){}
     ~Point()
     {
-        printf("деструктор 0x%x\n",this);
+        printf("╨┤╨╡╤Б╤В╤А╤Г╨║╤В╨╛╤А 0x%x\n",this);
     }
 };
 
 int task5()
 {
-    Pool<Point> pp(10); // выделена память под 10 объектов Point
-    Point &p1 = pp.alloc(); // сработал конструктор Point()
-    printf("p1 0x%x\n",&p1);
-    Point &p2 = pp.alloc(0, 1); // сработал конструктор Point(0, 1)
+    Pool<Point> pp(5); 
+    Point &p1 = pp.alloc(); 
+    printf("p1 0x%x\n",&p1);    
+    Point &p2 = pp.alloc(0, 1); 
     printf("p2 0x%x\n",&p2);
+    Point &p3 = pp.alloc(2, 4); 
+    printf("p3 0x%x\n",&p3);
+    Point &p4 = pp.alloc(4, 5); 
+    printf("p4 0x%x\n",&p4);
+    Point &p5 = pp.alloc(6, 7);
+    printf("p5 0x%x\n",&p5);
+    pp.free(3);
+    Point &p6 = pp.alloc(7, 8); 
+    printf("p6 0x%x\n",&p6);
+    pp.free();
     return 1;
 }
