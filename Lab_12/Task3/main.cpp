853cf5f7bebefeccc086cbb1987cae7dec0eeedf
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-04-19 18:24:55 +0700

    Update main.cpp

--- Lab_12/Task3/main.cpp
+++ Lab_12/Task3/main.cpp
@@ -5,7 +5,8 @@
 #include "safemap.h"
 #include <mutex>
 #include <chrono>
-#define MULIREAD
+#include <getopt.h>
+//#define MULIREAD
 
 std::mutex readMutex;
 SafeMap safemap;
@@ -14,7 +15,9 @@ int SafeMap::count;
 //2 варианта чтения последовательное каждым потоком или многопоточное
 void processRead(const std::shared_ptr<std::ifstream>& file_ptr,const std::string& path, size_t start, size_t end,const std::string& StrFind)
 {
+    readMutex.lock();
     auto timestart=std::chrono::high_resolution_clock::now();
+    readMutex.unlock();
     size_t findSize = StrFind.size();
     if (start + findSize > end)
     {
@@ -48,6 +51,7 @@ void processRead(const std::shared_ptr<std::ifstream>& file_ptr,const std::strin
     readMutex.unlock();
 #endif
     size_t pos = 0;
+
     while((pos = buffer.find(StrFind, pos)) != std::string::npos)
     {
         safemap.insert(start + pos);
@@ -55,19 +59,70 @@ void processRead(const std::shared_ptr<std::ifstream>& file_ptr,const std::strin
     }
     readMutex.lock();
     auto timeend=std::chrono::high_resolution_clock::now();
-    std::cout <<"potok nomer "<<std::this_thread::get_id()<<" srabotal za "<<std::chrono::duration_cast<std::chrono::milliseconds>(timeend - timestart).count()<<" millisec \n";
+    std::cout <<"potok nomer "<<std::this_thread::get_id()<<" srabotal za "<<std::chrono::duration_cast<std::chrono::nanoseconds>(timeend - timestart).count()<<" nanosec \n";
     readMutex.unlock();
 }
-int main()
+void PritnfHelp()
 {
+    std::wcout << L"Использование: ./programm [опции]\n"
+               << L"Опции:\n"
+               << L"  -p, --path     имя файла\n"
+               << L"  -t, --threads  количество потоков (кратное 2)\n"
+               << L"  -f, --fild     строка для поиска\n"
+               << L"  -c, --chunk    размер порции чтения\n"
+               << L"  -h, --help     показать эту справку\n";
+}
+//-p "D:\Lads\Task3\out.txt" -t 8 -f "работа" -c 32
+int main(int argc, char *argv[] )
+{
+    setlocale(LC_ALL, "");
     //размер блоков для обработки
     int sizeblock=32;
     //количество потоков
-    int CountThread=6;
-    std::string direct="C:\\test\\";
-    std::string path="out.txt";
+    int CountThread=8;
+    std::string path;
     //строка для поиска
     std::string  strfild="работ";
+
+    static struct option long_options[] = {
+    {"path",     required_argument, 0, 'p'},
+    {"threads",  required_argument, 0, 't'},
+    {"fild",     required_argument, 0, 'f'},
+    {"chunk",    required_argument, 0, 'c'},
+    {"help",     no_argument,       0, 'h'},
+    {0, 0, 0, 0}
+    };
+    int opt;
+    while ((opt = getopt_long(argc, argv, "p:t:f:c:h", long_options, NULL)) != -1) {
+        switch (opt) {
+        case 'p':
+            path = optarg;
+            break;
+        case 't':
+            CountThread = atoi(optarg);
+            break;
+        case 'f':
+            strfild = optarg;
+            break;
+        case 'c':
+            sizeblock = atoi(optarg);
+            break;
+        case 'h':
+            PritnfHelp();
+            return 0;
+        default:
+            PritnfHelp();
+            return 1;
+        }
+    }
+    if (path.empty() || CountThread <= 0 || strfild.empty() || sizeblock <= 0) {
+        PritnfHelp();
+        return 1;
+    }
+    if (CountThread % 2 != 0) {
+        std::wcout << L"Количество потоков должно быть кратным 2\n";
+        return 1;
+    }
     std::vector<std::future<void>> futures;
     std::shared_ptr<std::ifstream> file_ptr=std::make_shared<std::ifstream>(path,std::ios::binary);
     if (!file_ptr->is_open())
