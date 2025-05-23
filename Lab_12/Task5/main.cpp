4da3eea6b10435082dffd3f21654b79f6bfb3bda
Artem1008 <45067087+Artem1008@users.noreply.github.com>
2025-05-23 10:29:35 +0700

    Update main.cpp

--- Lab_12/Task5/main.cpp
+++ Lab_12/Task5/main.cpp
@@ -1,9 +1,11 @@
 #include "Model.h"
-
+//Controller
 int main()
 {
     setlocale(LC_ALL, "");
+    //View
     TrafficLight trafficlight;
+    //Model
     Model model(&trafficlight);
     model.run();
     return 0;
